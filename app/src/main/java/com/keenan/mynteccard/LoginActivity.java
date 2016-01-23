0145ecefd10fae84c84b96f52365464d79353f10
kgcom <kgcomia@gmail.com>
2016-01-24 09:56:55 +1300

    With Sign in and Registration function. 24- Jan-16

--- app/src/main/java/com/keenan/mynteccard/LoginActivity.java
+++ app/src/main/java/com/keenan/mynteccard/LoginActivity.java
@@ -4,16 +4,19 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,18 +24,14 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-import app.AppConfig;
-import app.AppController;
-import helper.SQLiteHandler;
-import helper.SessionManager;
-
 public class LoginActivity extends AppCompatActivity {
     EditText etemail , etpassword;
     private Button btnLogin;
-    private static final String TAG = RegisterActivity.class.getSimpleName();
-    private ProgressDialog pDialog;
-    private SessionManager session;
-    private SQLiteHandler db;
+    TextView tvregister;
+    private RequestQueue requestQueue;
+    private static final String URL = "http://192.168.1.4/jsonmysql/login.php";
+    private StringRequest request;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,135 +39,61 @@ public class LoginActivity extends AppCompatActivity {
         etemail=(EditText)findViewById(R.id.etLoginEmail);
         etpassword=(EditText)findViewById(R.id.etLoginPassword);
         btnLogin=(Button)findViewById(R.id.btnLogin);
-        // Progress dialog
-        pDialog = new ProgressDialog(this);
-        pDialog.setCancelable(false);
+        requestQueue = Volley.newRequestQueue(this);
 
+        tvregister=(TextView)findViewById(R.id.tvGoToRegister);
         // SQLite database handler
-        db = new SQLiteHandler(getApplicationContext());
 
-        // Session manager
-        session = new SessionManager(getApplicationContext());
-
-        if (session.isLoggedIn()) {
-            // User is already logged in. Take him to main activity
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
         btnLogin.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
-               String email=etemail.getText().toString().trim();
-               String password=etpassword.getText().toString().trim();
+            public void onClick(View view) {
+                // Toast.makeText(getApplication(),"Poop",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject.names().get(0).equals("success")){
+                                Toast.makeText(getApplicationContext(),"SUCCESS:"+jsonObject.getString("success"),Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            }else {
+                                Toast.makeText(getApplicationContext(), "Error: " +jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
+                            }
 
-                if (!email.isEmpty()&&!password.isEmpty()){
-                    loginUser(email,password);
-                }else{
-                    Toast.makeText(getApplicationContext(),"Please fill out required fields",Toast.LENGTH_SHORT).show();
-                }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String,String> hashMap = new HashMap<String, String>();
+                        hashMap.put("email",etemail.getText().toString());
+                        hashMap.put("password",etpassword.getText().toString());
+
+                        return hashMap;
+                    }
+                };
+
+                requestQueue.add(request);
             }
         });
     }
 
-    private void loginUser(final String email, final String password) {
-        String tag_string_req = "req_login";
-
-        pDialog.setMessage("Logging in ...");
-        showDialog();
-
-        StringRequest strReq = new StringRequest(Request.Method.POST,
-                AppConfig.URL_LOGIN, new Response.Listener<String>() {
-
-            @Override
-            public void onResponse(String response) {
-                Log.d(TAG, "Login Response: " + response.toString());
-                hideDialog();
-
-                try {
-                    JSONObject jObj = new JSONObject(response);
-                    boolean error = jObj.getBoolean("error");
-
-                    // Check for error node in json
-                    if (!error) {
-                        // user successfully logged in
-                        // Create login session
-                        session.setLogin(true);
-
-                        // Now store the user in SQLite
-                        String uid = jObj.getString("uid");
-
-                        JSONObject user = jObj.getJSONObject("user");
-                        String name = user.getString("name");
-                        String email = user.getString("email");
-                        String created_at = user
-                                .getString("created_at");
-
-                        // Inserting row in users table
-                        db.addUser(name, email, uid, created_at);
-
-                        // Launch main activity
-                        Intent intent = new Intent(LoginActivity.this,
-                                MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        // Error in login. Get the error message
-                        String errorMsg = jObj.getString("error_msg");
-                        Toast.makeText(getApplicationContext(),
-                                errorMsg, Toast.LENGTH_LONG).show();
-                    }
-                } catch (JSONException e) {
-                    // JSON error
-                    e.printStackTrace();
-                    Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "Login Error: " + error.getMessage());
-                Toast.makeText(getApplicationContext(),
-                        error.getMessage(), Toast.LENGTH_LONG).show();
-                hideDialog();
-            }
-        }) {
-
-            @Override
-            protected Map<String, String> getParams() {
-                // Posting parameters to login url
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("email", email);
-                params.put("password", password);
-
-                return params;
-            }
-
-        };
-
-        // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
-    }
-
-    private void showDialog() {
-        if (!pDialog.isShowing())
-            pDialog.show();
-    }
-
-    private void hideDialog() {
-        if (pDialog.isShowing())
-            pDialog.dismiss();
-    }
 
 
-    public void registerUser(View v) {
-        Intent intent = new Intent(this,RegisterActivity.class);
+
+
+    public void register(View view){
+        Intent intent = new Intent (this,RegisterActivity.class);
         startActivity(intent);
-
     }
-
-
 }
--- app/src/main/java/com/keenan/mynteccard/MainActivity.java
+++ app/src/main/java/com/keenan/mynteccard/MainActivity.java
@@ -1,7 +1,10 @@
 package com.keenan.mynteccard;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,6 +16,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -69,10 +76,43 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Date now = new Date();
+            android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+            try {
+                // image naming and path  to include sd card  appending name you choose for file
+                String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+                // create bitmap screen capture
+                View v1 = getWindow().getDecorView().getRootView();
+                v1.setDrawingCacheEnabled(true);
+                Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+                v1.setDrawingCacheEnabled(false);
+
+                File imageFile = new File(mPath);
+
+                FileOutputStream outputStream = new FileOutputStream(imageFile);
+                int quality = 100;
+                bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+                outputStream.flush();
+                outputStream.close();
+
+                openScreenshot(imageFile);
+            } catch (Throwable e) {
+                // Several error may come out with file handling or OOM
+                e.printStackTrace();
+            }
+
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
